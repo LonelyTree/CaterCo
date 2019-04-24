22206e4c5bea105006bbcd43412fe147384f120e
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-04-24 11:02:32 -0700

    server update

--- Controllers/userCtrl.js
+++
@@ -1,9 +0,0 @@
-const User = require("../models/User");
-
-index: (req, res) => {
-    User.find({}, (err, allUsers) => {
-        res.render("arrivalPage.ejs", {
-            user: allUsers
-        })
-    })
-}
--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const session = require("express-session");
+const userRouter = require("./Routes/userRoute");
+const mainRouter = require("./Routes/mainRoute");
+const adminRouter = require("./Routes/adminRoute");
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride("_method"));
 app.use(logger("dev"));
+
+app.use("/userRoute", userRouter);
+app.use("/mainRoute", mainRouter);
+app.use("/adminRoute", adminRouter);
 
 
 
