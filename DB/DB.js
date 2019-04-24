15fa30b8122bdd834ede8cc770d850a31829740c
Alex Hughes <alexhugh121@gmail.com>
2019-04-24 09:34:50 -0700

    Added Skeleton

---
+++ DB/DB.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost/CaterCo';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('mongoose connected to ', connectionString);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('mongoose disconnected to ', connectionString);
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('mongoose error ', error);
+});
