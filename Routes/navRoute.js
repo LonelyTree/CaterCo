2db470def9f3685279a2c6daa23afe54b063a70d
Alex Hughes <alexhugh121@gmail.com>
2019-04-25 09:19:49 -0700

    Main Update Route Requires

--- Routes/navRoute.js
+++ Routes/navRoute.js
@@ -1,10 +1,7 @@
 require('../db/db')
 const express = require("express")
 const router = express.Router()
-const admin = require('../Controllers/adminCtrl')
 const nav = require('../Controllers/navCtrl')
-const user = require('../Controllers/userCtrl')
-const main = require('../Controllers/mainCtrl')
 
 
 router.get('/info', nav.info)
