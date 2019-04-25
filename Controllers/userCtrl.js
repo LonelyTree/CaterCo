2fa077708f49f2113b1440a9f155004dffb48b2b
Ethan <N/A>
2019-04-25 15:40:31 -0700

    add new order page and route

--- Controllers/userCtrl.js
+++ Controllers/userCtrl.js
@@ -12,9 +12,15 @@ const thankyou = (req, res) => {
     res.render('../Views/User/thankYou.ejs')
 }
 
+const neworder = (req,res)=>{
+    res.render('../Views/User/newOrder.ejs')
+}
+
+
 
 module.exports = {
     main,
     confirm,
-    thankyou
+    thankyou,
+    neworder
 }
