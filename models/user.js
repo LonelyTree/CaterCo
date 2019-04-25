032f183b2c932c03a205e7c64697e556df94d784
Alex Hughes <alexhugh121@gmail.com>
2019-04-25 15:21:22 -0700

    Added Auth to Login and Register. Account passwords fully hashed and salted

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Food = require('./food')
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    payment: { type: Number, required: true },
+    payment: { type: String, required: true },
     email: { type: String, required: true },
     orders: [{
         date: { type: String },
