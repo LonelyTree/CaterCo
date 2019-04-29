d59128dacfbdb85583ff0e7a58c6a95c666623d8
Alex Hughes <alexhugh121@gmail.com>
2019-04-29 16:16:31 -0700

    Changed Incompatable Routes in Ctrl pages.

--- models/const.js
+++ models/const.js
@@ -4,20 +4,19 @@ const mongoose = require("mongoose");
 
 
 
-const Constants = {
-
-  
-Date:{
-  d:new Date(),
-  year:d.getFullYear(),
-  month:d.getMonth()+1,
-  day:d.getDate(),
-  Today:`${year}-${month}-${day}`
-},
+const Constant = {
 
 
-Categories: ["Salads", "Appetizers", "Sandwiches", "Big Entrees", "Drinks"],
+    // Date: {
+    //     d: new Date(),
+    //     year: d.getFullYear(),
+    //     month: d.getMonth() + 1,
+    //     day: d.getDate(),
+    //     Today: `${year}-${month}-${day}`
+    // },
+
+
+    Categories: ["Salads", "Appetizers", "Sandwiches", "Big Entrees", "Drinks"],
 
 }
 module.exports = Constant
-
